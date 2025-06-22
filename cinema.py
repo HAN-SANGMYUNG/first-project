577848b6d29df812a81eb2d21ff3cf2572cbb8bd
Han sang myung <tkdauda8013@gmail.com>
2025-06-22 18:17:18 +0900

    second commit

--- cinema.py
+++ cinema.py
@@ -1,13 +1,14 @@
 # cinema.py
 from store_module import run_store
-
+#수정하는거 알아보기기
 product_catalog = {
     "1": ("🎟️ Standard Ticket", 12.00),
     "2": ("🥤 Large Drink", 5.50),
-    "3": ("🍿 Popcorn Combo", 8.25)
+    "3": ("🍿 Popcorn Combo", 8.25),
+    "4": ("ddsdsd", 32)
 }
 store_name = "Grand Cinema"
 unit_name = "item(s)"
-
+#안녕하세요요
 if __name__ == "__main__":
     run_store(store_name, product_catalog, unit_name)
